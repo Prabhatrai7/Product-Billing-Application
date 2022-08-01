03b79ab04b94d2b03e64178d8337142e770353ad
Prabhat Ranjan Rai <84306814+Prabhatrai7@users.noreply.github.com>
2022-08-02 01:33:25 +0530

    Add files via upload

---
+++ ProductBillingApplication/src/main/java/com/cts/dao/ProductDaoImpl.java
@@ -0,0 +1,32 @@
+package com.cts.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import com.cts.bean.Product;
+
+@Repository
+public class ProductDaoImpl implements ProductDao {
+
+	//wire dependency
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Override
+	public Product getProductDetails(int product_code) {
+		String query = "select * from product where product_code = ?";
+		return jdbcTemplate.queryForObject(query, new Object[] { product_code }, new RowMapper<Product>() {
+
+			@Override
+			public Product mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return new Product(rs.getInt("product_code"), rs.getString("product_name"), rs.getString("product_category"), rs.getString("product_desc"), rs.getDouble("product_price"));
+			}
+		});
+	}
+}
